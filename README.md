4283c9aeba0fe2bd7e8f9ae2dbdc7eb3d3687dd5
little-bear <little-bear-x@outlook.com>
2024-11-23 14:24:27 +0800

    0.4.0

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-项目介绍
+# 项目介绍
 此项目为rust-craft-2d的启动器 请前往此项目查看项目内容 [github](https://github.com/little-bear-x/rust-craft-2d) [gitee](https://gitee.com/little-bear-2009/rust-craft-2d/)
+
+# 更新内容
+# 0.4.0
+1. [新增]新增了地图种子设置
+2. [新增]新增了删除存档
+3. [优化]优化了使用体验
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 from PySide6.QtUiTools import QUiLoader
-from PySide6.QtWidgets import QApplication
+from PySide6.QtWidgets import QApplication, QMessageBox
 from PySide6.QtCore import Qt, QThread, Signal, Slot
 from PySide6.QtCore import QFile, QIODevice, QObject
 
@@ -8,8 +8,9 @@ from pathlib import Path
 import platform
 import subprocess
 import os
+import random
 
-LAUNCHER_VERSION = "rust-craft-2d 0.3.3 (demo)"
+LAUNCHER_VERSION = "rust-craft-2d 0.4.0 (demo)"
 
 class GameThread(QThread):
     update_game_data = Signal(str, int)
@@ -42,28 +43,46 @@ class MainWindow:
 
         self.ui.setFixedSize(800, 600)
 
-        # 从UI定义中动态创建一个相应的窗口对象
-        saved_game_path = Path("SavedGames/")
-        files = list(saved_game_path.rglob(f"*game"))
-        for file in files:
-            self.ui.open_game_name.addItem(str(file.stem))
+        self.update_saved_games()
         
         # 按钮信号槽
-        self.ui.open_game_button_box.accepted.connect(self.open_game)
-        self.ui.open_game_button_box.rejected.connect(self.exit)
-        self.ui.new_game_button_box.accepted.connect(self.new_game)
-        self.ui.new_game_button_box.rejected.connect(self.exit)
+        self.ui.new_game_start_button.clicked.connect(self.new_game)
+        self.ui.open_game_update_list.clicked.connect(self.update_saved_games)
+        self.ui.open_game_start_button.clicked.connect(self.open_game)
+        self.ui.open_game_delete_button.clicked.connect(self.delete_game)
 
         # 新建游戏选择框信号槽
         self.ui.new_game_type_sandbox.clicked.connect(lambda: self.change_game_type("sandbox"))
         self.ui.new_game_type_survival.clicked.connect(lambda: self.change_game_type("survival"))
 
+        # Tab信号槽
+        self.ui.main_tab.currentChanged.connect(self.update_saved_games)
+
+        # 随即seed按钮信号槽
+        self.ui.new_game_seed_random.clicked.connect(self.random_seed)
+
     def exit(self):
         self.ui.close()
 
+    def random_seed(self):
+        self.ui.new_game_seed_input.setText(str(random.randint(0, 2147483647)))
+
     def new_game(self):
+        print(self.ui.new_game_seed_input.text())
         if self.ui.new_game_name.text() == "":
             self.ui.new_game_log.setText(self.ui.new_game_log.toPlainText() + "[launcher]panic: game name can not be empty\n")
+            return
+
+        if self.ui.new_game_seed_input.text() == "":
+            self.ui.new_game_log.setText(self.ui.new_game_log.toPlainText() + "[launcher]panic: seed can not be empty\n")
+            return
+        try:
+            game_seed = int(self.ui.new_game_seed_input.text())
+        except ValueError:
+            self.ui.new_game_log.setText(self.ui.new_game_log.toPlainText() + "[launcher]panic: seed must be a number\n")
+            return
+        if game_seed < -1 or game_seed > 2147483647:
+            self.ui.new_game_log.setText(self.ui.new_game_log.toPlainText() + "[launcher]panic: seed must be between -1 and 2147483647\n")
             return
     
         system = platform.system()
@@ -73,22 +92,35 @@ class MainWindow:
             if LAUNCHER_VERSION != game_version:
                 self.ui.new_game_log.setText(self.ui.new_game_log.toPlainText() + "[launcher]warn: The launcher version does not match the game version! This may cause errors!\n")
                 
-            msg = "[launcher]info: running command: ./rust-craft-2d.exe --new " + self.ui.new_game_name.text() + " --gametype " + self.new_game_type
+            msg = "[launcher]info: running command: ./rust-craft-2d.exe --new " + self.ui.new_game_name.text() + " --gametype " + self.new_game_type + " --seed " + str(game_seed)
             self.ui.new_game_log.setText(self.ui.new_game_log.toPlainText() + msg + "\n")
-            self.start_game("./rust-craft-2d.exe --new " + self.ui.new_game_name.text() + " --gametype " + self.new_game_type, 0)
+            self.start_game("./rust-craft-2d.exe --new " + self.ui.new_game_name.text() + 
+                                " --gametype " + self.new_game_type + 
+                                " --seed " + str(game_seed), 
+                            0)
         elif system == "Linux":
             game_version = os.popen("./rust-craft-2d --version").read().splitlines()[0]
             self.ui.new_game_log.setText(self.ui.new_game_log.toPlainText() + "[launcher]info Starting...game version: " + game_version + " | launcher version: " + LAUNCHER_VERSION + "\n")
             if LAUNCHER_VERSION != game_version:
                 self.ui.new_game_log.setText(self.ui.new_game_log.toPlainText() + "[launcher]warn: The launcher version does not match the game version! This may cause errors!\n")
 
-            msg = "[launcher]info: running command: ./rust-craft-2d --new " + self.ui.new_game_name.text() + " --gametype " + self.new_game_type
+            msg = "[launcher]info: running command: ./rust-craft-2d --new " + self.ui.new_game_name.text() + " --gametype " + self.new_game_type + " --seed " + str(game_seed)
             self.ui.new_game_log.setText(self.ui.new_game_log.toPlainText() + msg + "\n")
-            self.start_game("./rust-craft-2d --new " + self.ui.new_game_name.text() + " --gametype " + self.new_game_type, 0)
+            self.start_game("./rust-craft-2d --new " + self.ui.new_game_name.text() + 
+                                " --gametype " + self.new_game_type + 
+                                " --seed " + str(game_seed), 
+                            0)
         else:
             msg = "[launcher]panic: unsupported platform: " + system
             self.ui.new_game_log.setText(self.ui.new_game_log.toPlainText() + msg + "\n")
     
+    def update_saved_games(self):
+        self.ui.open_game_name.clear()
+        saved_game_path = Path("SavedGames/")
+        files = list(saved_game_path.rglob(f"*game"))
+        for file in files:
+            self.ui.open_game_name.addItem(str(file.stem))
+
     def open_game(self):
         if self.ui.open_game_name.currentItem() == None:
             self.ui.open_game_log.setText(self.ui.open_game_log.toPlainText() + "[launcher]panic: game name can not be empty\n")
@@ -115,7 +147,20 @@ class MainWindow:
         else:
             msg = "[launcher]panic: unsupported platform: " + system
             self.ui.open_game_log.setText(self.ui.open_game_log.toPlainText() + msg + "\n")
-    
+
+    def delete_game(self):
+        if self.ui.open_game_name.currentItem() == None:
+            self.ui.open_game_log.setText(self.ui.open_game_log.toPlainText() + "[launcher]panic: game name can not be empty\n")
+            return
+        reply = QMessageBox.question(None, "删除存档?", "确定删除该存档?\n此操作不可逆!", QMessageBox.Yes | QMessageBox.No)
+        if reply == QMessageBox.Yes:
+            try:
+                os.remove("SavedGames/"+self.ui.open_game_name.currentItem().text()+".game")
+                self.ui.open_game_log.setText(self.ui.open_game_log.toPlainText() + "[launcher]info: Delete success.\n")
+            except:
+                self.ui.open_game_log.setText(self.ui.open_game_log.toPlainText() + "[launcher]panic: Cannot delete the archive! Please refresh the archive list.\n")
+        self.update_saved_games()
+
 
     def change_game_type(self, game_type):
         # print("change game type to "+game_type)
